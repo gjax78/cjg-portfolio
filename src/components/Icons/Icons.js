b2470c5cd211d3316768de6d0913aa7ba9b2ddc7
Geena Jackson <gjacks303@gmail.com>
2022-07-14 09:29:49 -0600

    Add links to icons

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -10,12 +10,24 @@ import linkedin from './assets/linkedin-icon.png'
 const Icons = () => {
   return (
     <section className='icon-container'>
-      <img src={email} alt='email-icon' className='email-icon' />
-      <img src={youtube} alt='youtube-icon' className='youtube-icon' />
-      <img src={tiktok} alt='tiktok-icon' className='tiktok-icon' />
-      <img src={instagram} alt='instagram-icon' className='instagram-icon' />
-      <img src={facebook} alt='facebook-icon' className='facebook-icon' />
-      <img src={linkedin} alt='linkedin-icon' className='linkedin-icon' />
+      <a href='mailto:somatic.fpv@gmail.com' target='blank'>
+        <img src={email} alt='email-icon' className='email-icon' />
+      </a>
+      <a href='https://www.youtube.com/channel/UCUxjbEtezPFLt733gmrh8qw' target='blank'>
+        <img src={youtube} alt='youtube-icon' className='youtube-icon' />
+      </a>
+      <a href='https://www.tiktok.com/@somatic.fpv' target='blank'>
+        <img src={tiktok} alt='tiktok-icon' className='tiktok-icon' />
+      </a>
+      <a href='https://www.instagram.com/somatic.fpv/?hl=en' target='blank'>
+        <img src={instagram} alt='instagram-icon' className='instagram-icon' />
+      </a>
+      <a href='https://m.facebook.com/somatic.cinema/' target='blank'>
+        <img src={facebook} alt='facebook-icon' className='facebook-icon' />
+      </a>
+      <a href='https://www.linkedin.com/in/cameron-goldberg-543710214/' target='blank'>
+        <img src={linkedin} alt='linkedin-icon' className='linkedin-icon' />
+      </a>
     </section>
   )
 }
