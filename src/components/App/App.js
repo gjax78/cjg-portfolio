77571d30557250cb8eeeddd2407ddff276a6820c
Geena Jackson <gjacks303@gmail.com>
2022-07-14 09:40:27 -0600

    Import About component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import LandingPage from '../LandingPage/LandingPage'
 import Icons from '../Icons/Icons';
-
+import About from '../About/About'
 const App = () => {
   return (
     <main className="App">
       <LandingPage />
       <Icons />
+      <About />
     </main>
   );
 }
