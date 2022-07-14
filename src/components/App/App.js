39f938ed8475c2c036f1109d2ffb52e5a06ede53
Geena Jackson <gjacks303@gmail.com>
2022-07-14 11:02:55 -0600

    Get Framer Motion working with youtube videos

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import LandingPage from '../LandingPage/LandingPage'
 import Icons from '../Icons/Icons';
 import About from '../About/About'
+import Videos from '../Videos/Videos';
+
 const App = () => {
   return (
     <main className="App">
       <LandingPage />
       <About />
       <Icons />
+      <Videos />
     </main>
   );
 }
